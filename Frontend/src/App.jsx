8193099da25b528f282fa3309a01aedf715906f5
Soumik Ghosh <soumikghosh09072004@gmail.com>
2025-02-23 23:13:56 +0530

    First copy Added

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+// import Sidebar from './Components/Sidebar/Sidebar'
+import Chatbot from './Components/ChatModule/Chatbot'
+
+
+function App() {
+
+  return (
+    <>
+        <Chatbot></Chatbot>
+    </>
+  );
+}
+
+export default App
